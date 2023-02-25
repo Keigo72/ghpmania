fc79cc36d35e171ced9aaeb701a019e580d21c7d
GHPmania <ghpmania123@gmail.com>
2023-02-25 00:53:36 +0000

    質問一覧表示

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -10,8 +10,25 @@
     <div class="links">
         <h3 class="border-bottom border-gray pb-2 mb-0">質問投稿一覧</h3>
         <p>{{ $hogee }}</p>
-        @foreach($questions as $q)
-          <a>{{ $q->title }}</a>
-        @endforeach
+    
+        <table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th>id</th>
+      <th>質問タイトル</th>
+      <th>投稿日時</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($questions as $q)
+        <tr>
+            <th scope="row">{{ $q->id}}</th>
+            <td>{{ $q->title}}</td>
+            <td>{{ $q->created_at->format("Y年m月d日 H:i")}}</td>
+        </tr>
+    @endforeach
+  </tbody>
+</table>
+
     </div>
 @endsection
